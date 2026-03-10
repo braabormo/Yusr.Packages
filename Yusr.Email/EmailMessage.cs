91bdb159140b36cfab1c7f2c07fd5519af86833a
Ahmed Mohamed <ahmedbkry204@gmail.com>
2026-03-10 13:51:06 +0300

    add- email package

---
+++ Yusr.Email/EmailMessage.cs
@@ -0,0 +1,31 @@
+﻿namespace Yusr.Email
+{
+    public class EmailMessage
+    {  
+        public string SenderEmail { get; set; } = string.Empty;
+        public string SenderName { get; set; } = "YusrBus";
+        public string SenderAppKey { get; set; } = string.Empty;
+        public string[] ReceiversEmailsList { get; set; } = [];
+
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+
+        public List<byte[]> FilesBytes { get; set; } = new List<byte[]>();
+
+        public EmailMessage()
+        {
+            
+        }
+
+        public EmailMessage(string senderEmail, string senderName, string senderAppKey, string[] receiversEmailsList, string subject, string body, List<byte[]> filesBytes)
+        {
+            SenderEmail = senderEmail;
+            SenderName = senderName;
+            SenderAppKey = senderAppKey;
+            ReceiversEmailsList = receiversEmailsList;
+            Subject = subject;
+            Body = body;
+            FilesBytes = filesBytes;
+        }
+    }
+}
---
+++ Yusr.Email/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+namespace Yusr.Email
+{
+    public class EmailService
+    {
+        public async Task SendAsync(EmailMessage emailDTO)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(emailDTO.SenderName, emailDTO.SenderEmail));
+            message.To.Add(new MailboxAddress(emailDTO.SenderName, emailDTO.SenderEmail));
+
+            FillReceivers(emailDTO.ReceiversEmailsList, ref message);
+
+            message.Subject = emailDTO.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = $"<h2>{emailDTO.Body}</h2>";
+            builder.HtmlBody += "<h3>المرفقات</h3>";
+
+            int fileCounter = 1;
+            foreach (var fileBytes in emailDTO.FilesBytes)
+            {
+                string fileName = $"Ticket_{fileCounter}.pdf";
+                builder.Attachments.Add(fileName, fileBytes, ContentType.Parse("application/pdf"));
+                fileCounter++;
+            }
+
+            message.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(emailDTO.SenderEmail, emailDTO.SenderAppKey);
+            var response = await smtp.SendAsync(message);
+            Console.WriteLine(response);
+            await smtp.DisconnectAsync(true);
+        }
+        private void FillReceivers(string[] receiversEmailsList, ref MimeMessage message)
+        {
+            foreach (var receiver in receiversEmailsList)
+            {
+                message.Bcc.Add(new MailboxAddress(name: "", address: receiver));
+            }
+        }
+    }
+}
